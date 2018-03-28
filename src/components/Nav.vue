48d58f9b5920ad04a827b80711a10d1fc525d68c
burrelle <burrelle@oregonstate.edu>
2018-03-27 21:19:19 -0700

    Init and homepage

---
+++ src/components/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+      <ul class="nav">
+          <router-link to="/" exact active-class="active">Home</router-link>
+          <router-link to="/battle" active-class="active">Battle</router-link>
+          <router-link to="/popular" active-class="active">Popular</router-link>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'nav-bar'
+}
+</script>
+
+<style>
+.nav{
+    display: flex;
+}
+
+.active {
+    font-weight: bold;
+}
+
+.nav li {
+    margin-right: 15px;
+}
+
+a {
+    text-decoration: none;
+    color: #d0021b;
+    padding-right: 8px;
+}
+</style>
